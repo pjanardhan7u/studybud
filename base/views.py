893009c384128c77b78259e5bf5472a2c64c60ca
P_Janardhan <pjanardhan7u@gmail.com>
2025-03-07 20:14:59 +0530

    creating messages done 3:22

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render , redirect
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic,Message
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -80,7 +80,18 @@ def home(request):
 def room(request,pk):
     room=Room.objects.get(id=pk)
     room_messages=room.message_set.all().order_by('-created')
-    context={'room':room,'room_messages':room_messages}
+    participants=room.participants.all()
+    if request.method== 'POST':
+        message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get("body")
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+    
+    
+    context={'room':room,'room_messages':room_messages,'participants':participants}
     return render(request,'base/room.html',context)
 
 

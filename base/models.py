41ed2567486a4610350e36b1aeec2d11720a9a0b
P_Janardhan <pjanardhan7u@gmail.com>
2025-03-07 15:14:32 +0530

    created topic and message models

--- base/models.py
+++ base/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+
+# topic can have multiple rooms a room can have one topic
+class Topic(models.Model):
+    name=models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.name
+
+
 
 class Room(models.Model):
-    # host=
-    # topic=
+    host=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
+    topic=models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     name=models.CharField(max_length=200)
     description=models.TextField(null=True,blank=True)
     # participants=
@@ -13,3 +25,13 @@ class Room(models.Model):
         return self.name
 
  
+#  Room and Message has one to many relation
+class Message(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    room =models.ForeignKey(Room,on_delete=models.CASCADE)
+    body = models.TextField()
+    updated=models.DateTimeField(auto_now=True)
+    created=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
+
 
 
 rooms=[
@@ -11,13 +13,11 @@ rooms=[
 
 
 def home(request):
+    rooms=Room.objects.all()
     return render(request,'base/home.html',{'rooms':rooms})
 
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
+    room=Room.objects.get(id=pk)
     context={'room':room}
     return render(request,'base/room.html',context)

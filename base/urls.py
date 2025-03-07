505a7178e3572b3a0b603e04c18856b1a7bac4cc
P_Janardhan <pjanardhan7u@gmail.com>
2025-03-07 22:36:23 +0530

    user profile page implemented

--- base/urls.py
+++ base/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('login/',views.loginPage,name="login"),
     path('logout/',views.logoutUser,name="logout"),
     
+    path('profile/<str:pk>',views.userProfile,name="user-profile"),
+    
     path('',views.home,name="home"),
     path('room/<str:pk>/',views.room,name="room"),
     path('create-room/',views.createRoom,name="create-room"),

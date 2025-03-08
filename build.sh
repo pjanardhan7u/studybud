0d68d3af82390d1ea6ea598fe77de6e7bcd76b82
P_Janardhan <pjanardhan7u@gmail.com>
2025-03-08 21:29:49 +0530

    updated collectsttic

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectionstatic --no-input
+python manage.py collectstatic --no-input
 python manage.py migrate
 
 if [[$CREATE_SUPERUSER]];

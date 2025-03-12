3251e17867ae016064eeedc60f9e4e6a1514451e
P_Janardhan <pjanardhan7u@gmail.com>
2025-03-12 14:19:20 +0530

    added mysql

--- studybud/settings.py
+++ studybud/settings.py
@@ -79,12 +79,26 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'learnspace',
+        'USER': 'root',
+        'PASSWORD': '0556',
+        'HOST': 'localhost',  # or the hostname where your MySQL server is running
+        'PORT': '3306',      # or the port on which your MySQL server is listening
     }
 }
+
+
 
 
 # Password validation
